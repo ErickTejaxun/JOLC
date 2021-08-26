dcaec0866e3603609f88a6e9816b81fa83c60542
ericktejaxun <erickteja@gmail.com>
2021-08-25 21:12:55 -0600

    Configuracion inicial heroku deploy

--- static/js/editor_conf.js
+++ static/js/editor_conf.js
@@ -1,15 +1,22 @@
 // referencia a codemirror: https://codemirror.net/doc/manual.html
 var editorInput = CodeMirror.fromTextArea(document.getElementById("default"),{
+    theme: "dracula",
+    mode:  "javascript",
+    value: "function myScript(){return 100;}\n",
     lineNumbers: true,
     styleActiveLine: true,
-    matchBrackets: true,
-    theme: "dracula"    
+    matchBrackets: true
+
 });
 
 
 var editorOutput = CodeMirror.fromTextArea(document.getElementById("output"),{
+    theme: '3024-night',
+    mode:  "javascript",
+    value: "function myScript(){return 100;}\n",
     lineNumbers: true,
-    theme: '3024-night'    
+    styleActiveLine: true,
+    matchBrackets: true  
 });
 
 
